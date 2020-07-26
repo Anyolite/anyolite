21ee041aac2790bda9c62e9c1031acef9f625542
Hadeweka <hadeweka@outlook.de>
2020-07-26 17:42:28 +0200

    Added format string module

--- src/mruby.cr
+++ src/mruby.cr
@@ -1,6 +1,9 @@
 require "./mruby_internal.cr"
+
 require "./mrb_state.cr"
 require "./mrb_class.cr"
 require "./mrb_cast.cr"
+
+require "./format_string.cr"
 
 alias MrbFunc = Proc(MRubyInternal::MrbState*, MRubyInternal::MrbValue, MRubyInternal::MrbValue)
