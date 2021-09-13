8f005401541043e661f5610109f2f3cebba058b8
Hadeweka <hadeweka@outlook.de>
2021-09-13 20:40:59 +0200

    Skip MRI implementation files in docs

--- src/implementations/mri/FormatString.cr
+++ src/implementations/mri/FormatString.cr
@@ -1,34 +1,36 @@
-{% if flag?(:anyolite_implementation_ruby_3) %}
-  module Anyolite
-    module Macro
-      macro format_string(args, context = nil)
-        {% if args %}
-          {% required_counter = 0 %}
-          {% optional_counter = 0 %}
-          {% optional_args = false %}
+{% unless flag?(:anyolite_implementation_ruby_3) %}
+  {% skip_file %}
+{% end %}
 
-          {% for arg in args %}
-            {% if arg.is_a?(TypeDeclaration) %}
-              {% if arg.value || optional_args %}
-                {% optional_counter += 1 %}
-                {% optional_args = true %}
-              {% else %}
-                {% required_counter += 1 %}
-              {% end %}
+module Anyolite
+  module Macro
+    macro format_string(args, context = nil)
+      {% if args %}
+        {% required_counter = 0 %}
+        {% optional_counter = 0 %}
+        {% optional_args = false %}
+
+        {% for arg in args %}
+          {% if arg.is_a?(TypeDeclaration) %}
+            {% if arg.value || optional_args %}
+              {% optional_counter += 1 %}
+              {% optional_args = true %}
             {% else %}
-              {% if optional_args %}
-                {% optional_counter += 1 %}
-              {% else %}
-                {% required_counter += 1 %}
-              {% end %}
+              {% required_counter += 1 %}
+            {% end %}
+          {% else %}
+            {% if optional_args %}
+              {% optional_counter += 1 %}
+            {% else %}
+              {% required_counter += 1 %}
             {% end %}
           {% end %}
-
-          "{{required_counter}}{{optional_counter > 0 ? optional_counter : "".id}}"
-        {% else %}
-          ""
         {% end %}
-      end
+
+        "{{required_counter}}{{optional_counter > 0 ? optional_counter : "".id}}"
+      {% else %}
+        ""
+      {% end %}
     end
   end
-{% end %}
+end
