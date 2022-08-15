8a6cf7efee0c89ce80e00c4cb611aed3d0ee229a
Philipp Schulz <hadeweka@outlook.de>
2022-08-15 16:58:21 +0200

    Update install.cr

--- install.cr
+++ install.cr
@@ -1,6 +1,6 @@
 {% if flag?(:win32) %}
   # TODO: Is there a better solution?
-  system("start /B rake build_shard && exit")
+  system("cmd /C rake build_shard && exit")
 {% else %}
   system("rake build_shard")
 {% end %}
