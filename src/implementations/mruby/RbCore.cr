2436475bc68e18686b8b8662a072d473072c7f05
Philipp Schulz <hadeweka@outlook.de>
2022-09-10 17:17:49 +0200

    Reverted previous change

--- src/implementations/mruby/RbCore.cr
+++ src/implementations/mruby/RbCore.cr
@@ -3,11 +3,7 @@ module Anyolite
     {% build_path = env("ANYOLITE_BUILD_PATH") ? env("ANYOLITE_BUILD_PATH") : "build" %}
     
     {% if flag?(:win32) %}
-      {% if compare_versions(Crystal::VERSION, "1.5.1") >= 0 %}
-        @[Link(ldflags: "#{__DIR__}/../../../{{build_path.id}}/mruby/lib/libmruby.lib Ws2_32.lib")]
-      {% else %}
-        @[Link(ldflags: "#{__DIR__}/../../../{{build_path.id}}/mruby/lib/libmruby.lib msvcrt.lib Ws2_32.lib")]
-      {% end %}
+      @[Link(ldflags: "#{__DIR__}/../../../{{build_path.id}}/mruby/lib/libmruby.lib msvcrt.lib Ws2_32.lib")]
       @[Link(ldflags: "#{__DIR__}/../../../{{build_path.id}}/glue/mruby/return_functions.obj")]
       @[Link(ldflags: "#{__DIR__}/../../../{{build_path.id}}/glue/mruby/data_helper.obj")]
       @[Link(ldflags: "#{__DIR__}/../../../{{build_path.id}}/glue/mruby/script_helper.obj")]
